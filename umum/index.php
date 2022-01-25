42ba8bdb2c1a750bfcb309ed973c9cc659e65140
Canistayarra <98409912+Canistayarra@users.noreply.github.com>
2022-01-25 23:54:32 +0800

    Add files via upload

---
+++ umum/index.php
@@ -0,0 +1,72 @@
+<?php require_once 'header.php'; ?>
+<div class="row">
+    <div class="col-12 p-5">
+      <h1 class="display-4" align="center">SEJARAH KAIN TENUN IKAT KHAS LAMAHOLOT</h1>
+      <p align="justify">
+          Seni pada saat menenun secara tradisional yang dilakukan oleh perempuan 
+          Lamaholot didapat melalui pengalaman dan juga diwariskan secara alamiah. Beberapa 
+          perempuan dari Lamaholot yang menjadi seniman tenun sejati dan alami melalui desain 
+          motif sarung yang ditenun. Motif tenun dari Lamaholot mengandung nilai-nilai 
+          kehidupan yang diyakini sebagai warisan yang bersumber dari Sang Kuasa (Tuhan). 
+          Konstruksi motif dalam tenun ikat merupakan hasil imajinasi kaum perempuan yang 
+          diwujudkan dalam ikatan dan susunan motif pada kumparan benang. Motif-motif dalam 
+          tenun ikat menjadi identitas masyarakat Lamaholot pada umumnya dan perempuan 
+          Lamaholot. Pada tradisi di masa lalu, perempuan dipandang dewasa dan dapat menikah 
+          jika sudah pandai menenun dan mempunyai sejumlah sarung hasil dari tenunan atau 
+          karya tangan sendiri. Hal ini merupakan profil kaum perempuan Lamaholot pada zaman 
+          dahulu. Lalu bagaimana dengan perempuan Lamaholot di zaman sekarang. Pertanyaan ini 
+          bagian dari reflektif yang dimaksud untuk memberikan motivasi kepada kaum perempuan 
+          Lamaholot agar dapat berjuang melestarikan warisan tenun ikat khas Lamaholot (Sam,. S, 
+          2019).
+        </p>
+        <p align="justify">
+          Proses pembuatan kain tenun ini juga masih menggunakan alat khusus untuk 
+          menenun. Jenis alat tenun yang dipakai adalah alat tenun tradisional atau alat tenun bukan 
+          mesin alat-alat tersebut hingga kini masih dimanfaatkan oleh pengrajin tenun setempat, 
+          walaupun perkembangan teknologi alat tenun telah ada.
+        </p>
+        <p align="justify">
+          Fungsi utama dari kain tenun ikat yaitu sebagai busana sehari-hari, sebagai busana 
+          yang dipakai pada tarian-tarian adat/upacara adat ataupun festival, sebagai alat 
+          penghargaan dan pemberian perkawinan (belis/ mas kawin), sebagai pakaian adat 
+          perkawinan, sebagai alat penghargaan dan pemberian dalam acara kematian, sebagai alat 
+          untuk denda adat dalam mengembalikan keseimbangan sosial yang terganggu akibat 
+          suatu pelanggaran adat, sebagai alat tukar dalam bidang ekonomi, sebagai prestise dalam 
+          strata sosial masyarakat, sebagai alat penghargaan kepada tamu yang datang, sebagai 
+          sumber pendapatan masyarakat 364 di pedesaan, sebagai pakaian resmi untuk Pegawai 
+          Negeri Sipil pada hari tertentu, sebagai busana yang dipakai dalam acara/ kegiatan 
+          keagamaan atau rohani, dan juga sebagai souvenir atau oleh-oleh khas atau kenang-kenangan bagi wisatawan(Jati, H. 2019)
+        </p>
+        <p align="justify">
+          Suku Lamaholot adalah salah satu suku di Indonesia yang ada di wilayah
+          Kabupaten Flores Timur, Tanjung Bunga, Pulau Adonara, Pulau Solor dan Pulau 
+          Lembata. Masyarakat suku Lamaholot berbicara dalam bahasa Lamaholot. Lamaholot 
+          memiliki banyak varian bahasa yang disebut Lamaholot dan dialeknya. Menurut 
+          masyarakat Lamaholot, pada awalnya mereka hanya memiliki satu bahasa, yaitu 
+          Lamaholot, tetapi dengan campuran populasi suku lainnya yang mempengaruhi 
+          penggunaan bahasa dalam kehidupan sehari-hari. Masyarakat Lamaholot sampai saat ini 
+          masih memegang teguh adat istiadat yang diwariskan secara turun-temurun dari nenek 
+          moyang mereka. Masyarakat adat di Kabupaten Lamaholot memiliki kearifan lokal 
+          menenun. Kain tenun yang dibuat oleh perempuan Lamaholot, Flores Timur diwarnai 
+          dengan berbagai daun dan akar. Semua warna menggunakan warna alami. Dengan cara 
+          seperti itu, warna pada kain tenun akan semakin menarik dan tidak pudar.
+        </p>
+        <p align="justify">
+          Dalam penelitian (Vatter, 1984), dalam buku Ata Kiwan menyebutkan bahwa 
+          masyarakat lamaholot pada umumnya memiliki keterikatan yang khas dengan Lewotanah
+          atau tempat tinggal. Kampung merupakan kelompok sosial terbesar dan kesadaran 
+          berkelompok hampir tidak melampaui batas kampung. Pola organisasi kampung selalu 
+          dibangun dengan semangat dan pemikiran tentang kohesi sosial yang berpangkat pada 
+          faktor genealogis. Masyarakat lamaholot memiliki warisan budaya dan warisan itu masih 
+          dipertahankan hingga saat ini melalui sikap dan ekspresi dalam tingkah laku kehidupan 
+          sehari-hari.
+        </p>
+    </div>
+</div>
+
+<?php require_once 'footer.php'; ?>
+
+<script type="text/javascript">
+  $('.nav-link').removeClass('active');
+  $('.menu-home').addClass('active');
+</script>
